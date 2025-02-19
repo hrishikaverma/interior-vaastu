dec3e2f95b6d92dc904c9bffd569480d8e219439
Hrishika verma <hrishikaverma2@gmail.com>
2025-02-19 17:14:54 +0530

    removed warnings

--- src/blog/blog.js
+++ src/blog/blog.js
@@ -8,24 +8,19 @@ import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
 
 export function Blog() {
+  const articles = [
+    { id: 1, img: article1, title: "Building Construction Work", date: "3 March 2023" },
+    { id: 2, img: article2, title: "Modern Interior Design Trends", date: "10 April 2023" },
+    { id: 3, img: article3, title: "Minimalist Home Decor Ideas", date: "18 May 2023" },
+    { id: 4, img: article4, title: "Sustainable Architecture Insights", date: "25 June 2023" }
+  ];
+
   const handleChange = (e) => {
-    const pClassList = e.target.parentElement;
-    if (e.target.classList.contains("notaken")) {
-      for (let i = 0; i < pClassList.childNodes.length; i++) {
-        if (pClassList.childNodes[i].className === "blog-art taken") {
-          pClassList.childNodes[i].className = "blog-art notaken";
-        }
-      }
-      e.target.classList.remove("notaken");
-      e.target.classList.add("taken");
-    } else if (e.target.classList.contains("taken")) {
-      for (let i = 0; i < pClassList.childNodes.length; i++) {
-        if (pClassList.childNodes[i].className === "blog-art taken") {
-          pClassList.childNodes[i].className = "blog-art notaken";
-        }
-      }
-    }
+    const activeArticle = document.querySelector(".blog-art.taken");
+    if (activeArticle) activeArticle.classList.remove("taken");
+    e.currentTarget.classList.add("taken");
   };
+
   return (
     <div className="blog">
       <div className="blog-header">
@@ -38,26 +33,21 @@ export function Blog() {
         <h1>Latest Post</h1>
         <div className="lNews">
           <div className="ln-img">
-            <img src={latestNews} alt="news"></img>
+            <img src={latestNews} alt="news" />
           </div>
           <div className="ln-text">
             <div className="ln-subtext">
               <h1>Low Cost Latest Invented Interior Designing Ideas</h1>
               <p>
-                Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae
-                turpis dignissim maximus.posuere in.
-                <br />
-                <br />
-                Contrary to popular belief. Lorem Ipsum is not simply random
-                text. It has roots in a piece of classica.
+                Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpis dignissim maximus.posuere in.
+                <br /><br />
+                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classica.
               </p>
             </div>
             <div className="ln-detail">
               <p className="ac-date">3 March 2023</p>
-              <Link to={`/blog-details`}>
-                <button>
-                  <IoIosArrowForward />
-                </button>
+              <Link to="/blog-details">
+                <button><IoIosArrowForward /></button>
               </Link>
             </div>
           </div>
@@ -67,102 +57,22 @@ export function Blog() {
       <div className="blogArticles">
         <h1>Articles & News</h1>
         <div className="blog-art-list">
-          <div className="blog-art notaken" onClick={handleChange}>
-            <div className="blog-art-header">
-              <img src={article1} alt="article"></img>
-            </div>
-            <div className="blog-art-content">
-              <p>Let’s Get Solution For Building Construction Work</p>
-              <div className="ba-detail">
-                <p className="ba-date">3 March 2023</p>
-                <Link to={`/blog-details`}>
-                  <button>
-                    <IoIosArrowForward />
-                  </button>
-                </Link>
+          {articles.map((article) => (
+            <div key={article.id} className="blog-art notaken" onClick={handleChange}>
+              <div className="blog-art-header">
+                <img src={article.img} alt="article" />
+              </div>
+              <div className="blog-art-content">
+                <p>{article.title}</p>
+                <div className="ba-detail">
+                  <p className="ba-date">{article.date}</p>
+                  <Link to={`/blog-details`}>
+                    <button><IoIosArrowForward /></button>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="blog-art notaken" onClick={handleChange}>
-            <div className="blog-art-header">
-              <img src={article1} alt="article"></img>
-            </div>
-            <div className="blog-art-content">
-              <p>Let’s Get Solution For Building Construction Work</p>
-              <div className="ba-detail">
-                <p className="ba-date">3 March 2023</p>
-                <Link to={`/blog-details`}>
-                  <button>
-                    <IoIosArrowForward />
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="blog-art notaken" onClick={handleChange}>
-            <div className="blog-art-header">
-              <img src={article1} alt="article"></img>
-            </div>
-            <div className="blog-art-content">
-              <p>Let’s Get Solution For Building Construction Work</p>
-              <div className="ba-detail">
-                <p className="ba-date">3 March 2023</p>
-                <Link to={`/blog-details`}>
-                  <button>
-                    <IoIosArrowForward />
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="blog-art notaken" onClick={handleChange}>
-            <div className="blog-art-header">
-              <img src={article1} alt="article"></img>
-            </div>
-            <div className="blog-art-content">
-              <p>Let’s Get Solution For Building Construction Work</p>
-              <div className="ba-detail">
-                <p className="ba-date">3 March 2023</p>
-                <Link to={`/blog-details`}>
-                  <button>
-                    <IoIosArrowForward />
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="blog-art notaken" onClick={handleChange}>
-            <div className="blog-art-header">
-              <img src={article1} alt="article"></img>
-            </div>
-            <div className="blog-art-content">
-              <p>Let’s Get Solution For Building Construction Work</p>
-              <div className="ba-detail">
-                <p className="ba-date">3 March 2023</p>
-                <Link to={`/blog-details`}>
-                  <button>
-                    <IoIosArrowForward />
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="blog-art notaken" onClick={handleChange}>
-            <div className="blog-art-header">
-              <img src={article1} alt="article"></img>
-            </div>
-            <div className="blog-art-content">
-              <p>Let’s Get Solution For Building Construction Work</p>
-              <div className="ba-detail">
-                <p className="ba-date">3 March 2023</p>
-                <Link to={`/blog-details`}>
-                  <button>
-                    <IoIosArrowForward />
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
